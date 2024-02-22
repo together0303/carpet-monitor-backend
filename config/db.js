757d0c2a611229a0b97d15bf713504175f5c83e1
denisagapov <denisagapov6666@gmail.com>
2024-02-23 00:54:02 +0100

    mongodb change

--- config/db.js
+++ config/db.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://denisagapov6666:ot7b4bwyk6QOAvuz@cluster0.yksarw0.mongodb.net/scraping', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            suppressReservedKeysWarning: true // Add this option to suppress the warning
+const connectDB = async() => {
+    mongoose.connect('mongodb+srv://denisagapov6666:ot7b4bwyk6QOAvuz@cluster0.yksarw0.mongodb.net/scraping')
+        .then(() => {
+            console.log('Connected to MongoDB');
+        })
+        .catch((error) => {
+            console.error('Error connecting to MongoDB:', error);
         });
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-    }
-};
-
+}
 module.exports = connectDB;
