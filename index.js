6e920eb999da11896ff2a30e689ed1488a1314cc
denisagapov <denisagapov6666@gmail.com>
2024-03-19 05:56:52 +0100

    change index.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express =  require('express');
 const cors = require("cors");
 const scarpingConnectDB = require("./config/db");
 
+
 const app = express();
 
 const routes = require("./routes/index");
